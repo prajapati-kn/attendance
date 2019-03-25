5b761defeb6fd09b8d8e4e33a3caa583977d794c
unknown <prajapati.kn@somaiya.edu>
2019-03-25 17:07:18 +0530

    database added

--- app/src/main/java/com/example/exam/att_101/add_student.java
+++ app/src/main/java/com/example/exam/att_101/add_student.java
@@ -4,28 +4,55 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class add_student extends AppCompatActivity {
+    DatabaseHelper myDb;
+    Button btnAddData;
+    EditText n1;
+    EditText r1;
+    EditText d1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+        myDb = new DatabaseHelper(this);
+
     }
 
     public void register(View view){
-        EditText n1 = (EditText)findViewById(R.id.editText6);
+       n1 = (EditText)findViewById(R.id.editText6);
         String  name = n1.getText().toString();
 
-        EditText r1 = (EditText)findViewById(R.id.editText7);
+         r1 = (EditText)findViewById(R.id.editText7);
         Integer roll = Integer.parseInt(r1.getText().toString());
 
-        EditText d1 = (EditText)findViewById(R.id.editText8);
+         d1 = (EditText)findViewById(R.id.editText8);
         String  dept = d1.getText().toString();
 
-        Intent myIntent = new Intent(this, teacher.class);
-        this.startActivity(myIntent);
+        btnAddData = (Button)findViewById(R.id.button5);
+        AddData();
 
+
+
+    }
+    public  void AddData() {
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(n1.getText().toString(),
+                                r1.getText().toString(),
+                                d1.getText().toString() );
+                        if(isInserted == true)
+                            Toast.makeText(add_student.this, "Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(add_student.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/example/exam/att_101/student_info.java
+++ app/src/main/java/com/example/exam/att_101/student_info.java
@@ -1,32 +1,59 @@
 package com.example.exam.att_101;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class student_info extends AppCompatActivity {
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
+        myDb = new DatabaseHelper(this);
+        TextView name = (TextView)findViewById(R.id.name);
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0) {
+            // show message
+           // showMessage("Error","Nothing found");
+            return;
+        }
 
-        TextView t1 = (TextView)findViewById(R.id.textView);
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            buffer.append("Id :"+ res.getString(0)+"\n");
+            buffer.append("Name :"+ res.getString(1)+"\n");
+            buffer.append("ROLL NO :"+ res.getString(2)+"\n");
+            buffer.append("DEPT :"+ res.getString(3)+"\n\n");
+            buffer.append("ATTENDANCE :"+ res.getString(4)+"\n\n");
+           name.setText(buffer.toString());
+        }
 
-      TextView v1 = (TextView)findViewById(R.id.textView);
-        v1.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Toast.makeText(student_info.this, "name of the student",Toast.LENGTH_LONG).show();
-                return false;
-            }
-        });
+        // Show all data
+        //showMessage("Data",buffer.toString());
+
+
+
     }
+
+   /* public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }*/
+
 }
